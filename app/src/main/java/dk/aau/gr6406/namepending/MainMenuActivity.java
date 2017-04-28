4d4d13eb16327b64051ae2beae6e33343ba2819f
Martin S. Jensen <msje14@student.aau.dk>
2017-04-28 15:05:38 +0200

    Initial commit .)

--- app/src/main/java/dk/aau/gr6406/namepending/MainMenuActivity.java
+++ app/src/main/java/dk/aau/gr6406/namepending/MainMenuActivity.java
@@ -2,6 +2,8 @@ package dk.aau.gr6406.namepending;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void clickButton(View view){
+
+        Toast.makeText(MainMenuActivity.this,"BØHH",Toast.LENGTH_LONG).show();
+
     }
 }
