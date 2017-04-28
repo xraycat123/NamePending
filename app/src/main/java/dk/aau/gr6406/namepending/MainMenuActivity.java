5b43b086702ff7a8da9266fc59bd85d30de27163
Martin S. Jensen <msje14@student.aau.dk>
2017-04-28 14:59:01 +0200

    Initial commit

---
+++ app/src/main/java/dk/aau/gr6406/namepending/MainMenuActivity.java
@@ -0,0 +1,13 @@
+package dk.aau.gr6406.namepending;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
